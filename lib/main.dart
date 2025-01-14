4cd57e5560bd58c6dcb45a6fadce815e83d0148c
ryuunderground <jaeking00001@gmail.com>
2025-01-15 02:15:18 +0900

    first flutter

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text("Helloflutter")),
+          body: Center(child: Text("Hello world!"))),
+    );
+  }
+}
